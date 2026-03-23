af250899294e4062f18378f63411dc6ee2f9bc7b
gaorui <80497507@qq.com>
2026-03-23 13:49:09 +0800

    .

--- Py/gen_rebuild_ip.py
+++ Py/gen_rebuild_ip.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 
-ip_dir = Path("D:/ZYNQ7020_AD9361/Rtl/TCL_IPS")
+ip_dir = Path("D:/ZYNQ7020_AD9361/Rtl/TCL_IPS/ip_tcl")
 out_file = Path("D:/ZYNQ7020_AD9361/Rtl/TCL_IPS/rebuild_ips.tcl")
 
 tcl_lines = []
 
 tcl_lines.append("puts \"Rebuilding IPs...\"")
+tcl_lines.append(f"set current_sript_dir  [file normalize [file dirname [info script]]]")
 tcl_lines.append("")
 
 for tcl_file in sorted(ip_dir.glob("*.tcl")):
     name = tcl_file.stem
     tcl_lines.append(f"puts \"Creating {name}\"")
-    tcl_lines.append(f"source ./ip_tcl/{tcl_file.name}")
+    tcl_lines.append(f"source [file join $current_sript_dir/ip_tcl/{tcl_file.name}]")
     tcl_lines.append("")
 
 tcl_lines.append("puts \"Generating targets...\"")
--- Rtl/TCL_IPS/rebuild_ips.tcl
+++ Rtl/TCL_IPS/rebuild_ips.tcl
@@ -1,67 +1,69 @@
 puts "Rebuilding IPs..."
+set current_sript_dir  [file normalize [file dirname [info script]]]
+puts "current_sript_dir: $current_sript_dir"
 
 puts "Creating AD_CLK_PLL"
-source ./ip_tcl/AD_CLK_PLL.tcl
+source [file join $current_sript_dir/ip_tcl/AD_CLK_PLL.tcl]
 
 puts "Creating cmpy_16X16"
-source ./ip_tcl/cmpy_16X16.tcl
+source [file join $current_sript_dir/ip_tcl/cmpy_16X16.tcl]
 
 puts "Creating complexMult_IPCore"
-source ./ip_tcl/complexMult_IPCore.tcl
+source [file join $current_sript_dir/ip_tcl/complexMult_IPCore.tcl]
 
 puts "Creating DDS_16X16"
-source ./ip_tcl/DDS_16X16.tcl
+source [file join $current_sript_dir/ip_tcl/DDS_16X16.tcl]
 
 puts "Creating DIV_ATT"
-source ./ip_tcl/DIV_ATT.tcl
+source [file join $current_sript_dir/ip_tcl/DIV_ATT.tcl]
 
 puts "Creating div_calfreq"
-source ./ip_tcl/div_calfreq.tcl
+source [file join $current_sript_dir/ip_tcl/div_calfreq.tcl]
 
 puts "Creating div_idendFreq"
-source ./ip_tcl/div_idendFreq.tcl
+source [file join $current_sript_dir/ip_tcl/div_idendFreq.tcl]
 
 puts "Creating FIFO_Data_Sync"
-source ./ip_tcl/FIFO_Data_Sync.tcl
+source [file join $current_sript_dir/ip_tcl/FIFO_Data_Sync.tcl]
 
 puts "Creating g_clock_mmcm"
-source ./ip_tcl/g_clock_mmcm.tcl
+source [file join $current_sript_dir/ip_tcl/g_clock_mmcm.tcl]
 
 puts "Creating ila_128Xbit"
-source ./ip_tcl/ila_128Xbit.tcl
+source [file join $current_sript_dir/ip_tcl/ila_128Xbit.tcl]
 
 puts "Creating ila_64Xbit"
-source ./ip_tcl/ila_64Xbit.tcl
+source [file join $current_sript_dir/ip_tcl/ila_64Xbit.tcl]
 
 puts "Creating Rx_ReSample_FIR_F2"
-source ./ip_tcl/Rx_ReSample_FIR_F2.tcl
+source [file join $current_sript_dir/ip_tcl/Rx_ReSample_FIR_F2.tcl]
 
 puts "Creating Rx_ReSample_FIR_F4_L1"
-source ./ip_tcl/Rx_ReSample_FIR_F4_L1.tcl
+source [file join $current_sript_dir/ip_tcl/Rx_ReSample_FIR_F4_L1.tcl]
 
 puts "Creating Rx_ReSample_FIR_F4_L2"
-source ./ip_tcl/Rx_ReSample_FIR_F4_L2.tcl
+source [file join $current_sript_dir/ip_tcl/Rx_ReSample_FIR_F4_L2.tcl]
 
 puts "Creating Rx_ReSample_FIR_F5"
-source ./ip_tcl/Rx_ReSample_FIR_F5.tcl
+source [file join $current_sript_dir/ip_tcl/Rx_ReSample_FIR_F5.tcl]
 
 puts "Creating Tx_ReSample_FIR_F2"
-source ./ip_tcl/Tx_ReSample_FIR_F2.tcl
+source [file join $current_sript_dir/ip_tcl/Tx_ReSample_FIR_F2.tcl]
 
 puts "Creating Tx_ReSample_FIR_F4_L1"
-source ./ip_tcl/Tx_ReSample_FIR_F4_L1.tcl
+source [file join $current_sript_dir/ip_tcl/Tx_ReSample_FIR_F4_L1.tcl]
 
 puts "Creating Tx_ReSample_FIR_F4_L2"
-source ./ip_tcl/Tx_ReSample_FIR_F4_L2.tcl
+source [file join $current_sript_dir/ip_tcl/Tx_ReSample_FIR_F4_L2.tcl]
 
 puts "Creating Tx_ReSample_FIR_F5"
-source ./ip_tcl/Tx_ReSample_FIR_F5.tcl
+source [file join $current_sript_dir/ip_tcl/Tx_ReSample_FIR_F5.tcl]
 
 puts "Creating vio_0"
-source ./ip_tcl/vio_0.tcl
+source [file join $current_sript_dir/ip_tcl/vio_0.tcl]
 
 puts "Creating xfft_IPCore"
-source ./ip_tcl/xfft_IPCore.tcl
+source [file join $current_sript_dir/ip_tcl/xfft_IPCore.tcl]
 
 puts "Generating targets..."
 
